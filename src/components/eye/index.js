89aa0ca5030a52596a705e05e9a718063c41d9f8
unknown <jan@fuechtener.eu>
2021-01-11 07:57:51 +0100

    first commit

---
+++ src/components/eye/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import logo from "../../assets/png/logo.png";
+import styles from "./eye.module.css";
+
+function Eye(props) {
+  return <div className={styles.container}>Eye</div>;
+}
+
+export default Eye;
---
+++ src/pages/home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Eye } from "../../components";
+import styles from "./home.module.css";
+
+function Home() {
+  return (
+    <div className={styles.container}>
+      <Eye />
+    </div>
+  );
+}
+
+export default Home;
